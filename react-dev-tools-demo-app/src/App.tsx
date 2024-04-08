c5427b8d40a3846da5d1d10f40e5047d8fcccd5d
Maibam Maneesana Singh <mmaneesanasingh@covalience.com>
2024-04-08 18:06:06 +0530

    added tailwindcss +  updated card component +  made changes to the whole UI

--- react-dev-tools-demo-app/src/App.tsx
+++ react-dev-tools-demo-app/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Card } from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Card />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-[90%] m-[auto]">
+        <div className="flex flex-col justify-center items-center mt-[50px]">
+          <h3 className="text-[24px] font-bold mb-[20px] text-indigo-800">
+            Dev Tools Session
+          </h3>
+          <Card
+            details="This is a card. It has a title and some details. You can click on it to learn more.      
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio maxime vero harum facilis tempore aliquid adipisci ullam nesciunt natus. Qui blanditiis velit perferendis rem. Sapiente illo quae optio ea nostrum."
+            title="Card Title"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- react-dev-tools-demo-app/src/components/Card.tsx
+++ react-dev-tools-demo-app/src/components/Card.tsx
@@ -1,5 +1,12 @@
-import "./style.css";
-
-export const Card = () => {
-  return <div className="card">Card Component</div>;
+interface CardProps {
+  title: string;
+  details: string;
+}
+export const Card = ({ title, details }: CardProps) => {
+  return (
+    <div className=" bg-slate-50 p-3 rounded-md  w-[400px] min-h-[120px] drop-shadow-md m-2 text-wrap">
+      <h3 className="p-2 text-2xl text-cyan-800">{title}</h3>
+      <p className="text-sm p-2  text-stone-600">{details}</p>
+    </div>
+  );
 };
